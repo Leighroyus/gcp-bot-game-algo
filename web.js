1cb282ae4ebfdfa82539f1955bff731e7d173a67
leigh.royus <leigh.royus@gmail.com>
2021-10-02 00:34:47 +1000

    bugger

--- web.js
+++ web.js
@@ -78,8 +78,7 @@ app.post('/', function (req, res) {
 
   //res.send(moves[Math.floor(Math.random() * moves.length)]);
 
-  console.log('WTF!?')
-  if (my_player_prev_x.toString() == my_player_x.toString())
+  if (my_player_prev_x == my_player_x)
   {
     console.log('X coords have not moved!')
     if (my_player_x != 0 & my_player_x != 5)
